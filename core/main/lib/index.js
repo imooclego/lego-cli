33ff5781de8bf270e94b832953f8a729653b0f7a
qyingkou <qyingkou@163.com>
2021-02-02 11:46:07 +0800

    core模块prepare阶段:降级用户sudo使用权限

--- core/main/lib/index.js
+++ core/main/lib/index.js
@@ -6,23 +6,25 @@ const colors = require("colors");
 const log = require("@imooc-lego/cli-utils-log");
 const pkg = require("../package.json");
 const { LOWEST_NODE_VERSION } = require("./const");
+const { version, geteuid } = process;
 
 function main(argv) {
   try {
     log.notice("CLI_VERSION", "v%s", chk_pkg_ver());
     log.notice("NODE_VERSION", "v%s", chk_node_ver());
+    log.notice("UID", "%s", chk_root());
   } catch (e) {
     log.error(e.message);
   }
 }
-
+/* 检查包版本 */
 function chk_pkg_ver() {
   return pkg.version;
 }
+/* 检查node版本
+ * 版本过低抛错给外层
+ */
 function chk_node_ver() {
-  // 获取当前node版本号
-  const { version } = process;
-  // 比对最低要求版本号
   if (semver.gte(version, LOWEST_NODE_VERSION)) {
     return semver.clean(version);
   } else {
@@ -30,4 +32,11 @@ function chk_node_ver() {
       colors.red(`当前nodejs版本过低,请安装${LOWEST_NODE_VERSION}以上版本`)
     );
   }
+}
+/* 检查当前用户的使用权限
+ * 如果当前用户的使用权限是sudo则降级为该用户权限
+ */
+function chk_root() {
+  require("root-check")();
+  return geteuid();
 }
